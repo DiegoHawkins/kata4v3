ec4c69ae356f4d0bb88de58f7faf006cf9a1e22f
usuario <usuario@10.22.143.41>
2016-11-04 10:50:03 +0000

    Kata4v1

---
+++ src/katav4/Katav4.java
@@ -0,0 +1,14 @@
+package katav4;
+
+import java.io.File;
+
+public class Katav4 {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\");
+        String[] names = file.list();
+        for (String name : names) {
+            System.out.println(name);            
+        }
+    }
+}
